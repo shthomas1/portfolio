328eeacac1fa4e610c75ca8459a208d2fb13f3c7
shthomas1 <142743785+shthomas1@users.noreply.github.com>
2025-05-14 01:33:28 -0500

    V.28

--- src/utils/techCategories.js
+++ src/utils/techCategories.js
@@ -2,37 +2,47 @@
 
 export const getTechCategory = (tech) => {
   tech = tech.toLowerCase().trim();
-  
-  if (tech.includes("c#") || 
-      tech.includes("rust") || 
-      tech.includes(".net") || 
-      tech.includes("asp")) {
+
+  if (
+    tech.includes("c#") ||
+    tech.includes("rust") ||
+    tech.includes(".net") ||
+    tech.includes("lua") ||
+    tech.includes("asp")
+  ) {
     return "backend";
-  } else if (tech.includes("mysql") || 
-            tech.includes("database")) {
+  } else if (tech.includes("mysql") || tech.includes("database")) {
     return "database";
-  } else if (tech.includes("machine learning") || 
-            tech.includes("gradient") ||
-            tech.includes("random forest") ||
-            tech.includes("ai")) {
+  } else if (
+    tech.includes("machine learning") ||
+    tech.includes("gradient") ||
+    tech.includes("random forest") ||
+    tech.includes("ai")
+  ) {
     return "data";
-  } else if (tech.includes("aes") || 
-            tech.includes("encrypt") || 
-            tech.includes("security") ||
-            tech.includes("pad")) {
+  } else if (
+    tech.includes("aes") ||
+    tech.includes("encrypt") ||
+    tech.includes("security") ||
+    tech.includes("pad")
+  ) {
     return "security";
-  } else if (tech.includes("development") || 
-            tech.includes("consulting") || 
-            tech.includes("solutions") || 
-            tech.includes("integration")) {
+  } else if (
+    tech.includes("development") ||
+    tech.includes("consulting") ||
+    tech.includes("solutions") ||
+    tech.includes("integration")
+  ) {
     return "service";
-  } else if (tech.includes("html") || 
-            tech.includes("css") || 
-            tech.includes("javascript") || 
-            tech.includes("js") || 
-            tech.includes("design")) {
+  } else if (
+    tech.includes("html") ||
+    tech.includes("css") ||
+    tech.includes("javascript") ||
+    tech.includes("js") ||
+    tech.includes("design")
+  ) {
     return "frontend";
   }
-  
+
   return "frontend"; // Default category
 };
